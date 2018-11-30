db9e1008761fc4e576437c7c3367f6f29114c5f4
David Forlen <david.forlen@telecomnancy.eu>
2018-11-30 22:41:06 +0100

    Meilleure gestion des sprites des classes héritant de Case, parce que l'héritage ça fait partie des trucs qu'on apprend en MOCI

--- src/waterSymbol/board/cases/Wall.java
+++ src/waterSymbol/board/cases/Wall.java
@@ -7,17 +7,6 @@ import waterSymbol.board.Case;
 
 public class Wall extends Case {
 	public Wall(int x, int y) {
-		super(x, y);
-		
-		Image sprite = null;
-		
-		try {
-			sprite = new Image("res/images/wall.png");
-		} catch (SlickException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		super.setSprite(sprite);
+		super(x, y, "case");
 	}
 }
