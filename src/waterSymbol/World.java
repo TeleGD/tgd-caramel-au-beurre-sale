e5936c2436d7179a96078b6453c19827ff7f6543
AmosGeorge <amos.george0@gmail.com>
2018-12-01 00:42:54 +0100

    Implantation de TeamBuilder dans le world

--- src/waterSymbol/World.java
+++ src/waterSymbol/World.java
@@ -37,7 +37,6 @@ public class World extends BasicGameState {
 		} else if (this.state == 2) {
 			this.resume (container, game);
 		}
-		builder = new TeamBuilder(container.getWidth(), container.getHeight(), new Player("Tristan"), new Player("Axel"));
 	}
 
 	@Override
@@ -59,17 +58,30 @@ public class World extends BasicGameState {
 			this.setState (1);
 			game.enterState (2, new FadeOutTransition (), new FadeInTransition ());
 		}
-		builder.update(container, game, delta);
+		
+		if (!builder.areTeamsReady()) {
+			// Construction des teams
+			builder.update(container, game, delta);
+		} else {
+			//TODO en jeu
+		}
+		
 	}
 
 	@Override
 	public void render (GameContainer container, StateBasedGame game, Graphics context) {
 		/* Méthode exécutée environ 60 fois par seconde */
-		builder.render(container, game, context);
+		if (!builder.areTeamsReady()) {
+			// Construction des teams
+			builder.render(container, game, context);
+		} else {
+			//TODO en jeu
+		}
 	}
 
 	public void play (GameContainer container, StateBasedGame game) {
 		/* Méthode exécutée une unique fois au début du jeu */
+		builder = new TeamBuilder(10, container, new Player("Tristan"), new Player("Axel"));
 	}
 
 	public void pause (GameContainer container, StateBasedGame game) {
