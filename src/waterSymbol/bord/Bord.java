1fe4daf451b2b52c3cc6006d5d38e82fbb3b4a56
Frédéric Venier <frederic.venier@telecomnancy.net>
2018-11-30 21:52:39 +0100

    classes Cases et Bords

---
+++ src/waterSymbol/bord/Bord.java
@@ -0,0 +1,11 @@
+package waterSymbol.bord;
+
+import java.util.*;
+
+public class Bord {
+	private List<Case> cases;
+	
+	public Bord() {
+		this.cases = new ArrayList<Case>() ;
+	}
+}
---
+++ src/waterSymbol/bord/Case.java
@@ -0,0 +1,30 @@
+package waterSymbol.bord;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public abstract class Case {
+	private int x;
+	private int y;
+	private Image sprite;
+	
+	public Case(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setSprite(Image sprite) {
+		this.sprite = sprite;
+	}
+	
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+		
+	}
+	
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		g.drawImage(sprite, x, y);
+	}
+}
