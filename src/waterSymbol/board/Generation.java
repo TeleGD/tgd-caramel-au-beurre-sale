79c9e6832a9cbe8f6fe4c935fa7c2e25165f632f
Frédéric Venier <frederic.venier@telecomnancy.net>
2018-12-01 03:12:09 +0100

    merge

--- src/waterSymbol/board/Generation.java
+++ src/waterSymbol/board/Generation.java
@@ -5,12 +5,13 @@ import waterSymbol.board.cases.*;
 public class Generation {
 	private static int width, height;
 	private static Case[][] cases;
-
+	private static float ratio;
+	
 	public static Board generate(int screenWidth, int screenHeight) {
 		width = 35;
 		height = 20;
+		ratio = (screenWidth/1920f)>=(screenHeight/1080f)?(screenWidth/1920f):(screenHeight/1080f);
 		
-		float ratio = (screenWidth/1920f)<(screenHeight/1080f)?(screenWidth/1920f):(screenHeight/1080f);
 		cases = new Case[height][width];
 
 		for (int i = 0; i < height; i++) {
@@ -47,19 +48,19 @@ public class Generation {
 	private static void generateVerticalWall(int j) {
 		int r = (int)(Math.random()*10+5);
 		for(int i=0; i<r ; i++) {
-			cases[i][j] = new Wall(j,i);
+			cases[i][j] = new Wall(j,i,ratio);
 			//cases[height-i-1][width-j-1] = new Wall(width-j-1,height-i-1);
 		}
 		
 		for(int i=(int)(r+2+Math.random()*(height-r-2)); i<height; i++) {
-			cases[i][j] = new Wall(j,i);
+			cases[i][j] = new Wall(j,i,ratio);
 			//cases[height-i-1][width-j-1] = new Wall(width-j-1,height-i-1);
 		}
 	}
 	
 	private static void generateWallBlock(int i, int j, int iteration) {
 		if(iteration<Math.random()*3+2) {
-			cases[i][j] = new Wall(j,i);
+			cases[i][j] = new Wall(j,i,ratio);
 
 			if(j-1>0 && !(cases[0][j-1] instanceof Wall) && !(cases[i][j-1] instanceof Wall)) generateWallBlock(i,j-1, iteration+1);
 			if(j+1<width-1 && !(cases[0][j+1] instanceof Wall) && !(cases[i][j+1] instanceof Wall)) generateWallBlock(i,j+1, iteration+1);
@@ -70,9 +71,9 @@ public class Generation {
 	
 	private static void createPath(int i, int j) {
 		if(j<width) {
-			cases[i][j] = new Floor(j,i);
-			if(i-1>0) cases[i-1][j] = new Floor(j,i-1);
-			if(i+1<width-1) cases[i+1][j] = new Floor(j,i+1);
+			cases[i][j] = new Floor(j,i,ratio);
+			if(i-1>0) cases[i-1][j] = new Floor(j,i-1,ratio);
+			if(i+1<width-1) cases[i+1][j] = new Floor(j,i+1,ratio);
 			
 			int r = (int)(Math.random()*5);
 			if(r==4 && i>=2) createPath(i-1,j);
