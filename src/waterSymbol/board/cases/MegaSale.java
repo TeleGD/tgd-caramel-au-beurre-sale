6958d9131a1fbdecac564284679dafeb2e1d2e7b
Frédéric Venier <frederic.venier@telecomnancy.net>
2018-12-01 04:42:19 +0100

    ajout des sales

---
+++ src/waterSymbol/board/cases/MegaSale.java
@@ -0,0 +1,8 @@
+package waterSymbol.board.cases;
+
+public class MegaSale extends Case {
+	public MegaSale(int x, int y, float ratio) {
+		super(x, y, "mega_sale", ratio);
+	}
+
+}
---
+++ src/waterSymbol/board/cases/Sale.java
@@ -0,0 +1,7 @@
+package waterSymbol.board.cases;
+
+public class Sale extends Case {
+	public Sale(int x, int y, float ratio) {
+		super(x, y, "sale", ratio);
+	}
+}
